551a6442509bb0fe3a0a67d74350cfc82bc523a5
Stephane Sezer <sas@cd80.net>
2012-03-05 23:06:24 -0800

    Implemented high RAM support (trivial commit in fact).

--- src/memory/ram.cpp
+++ src/memory/ram.cpp
@@ -3,6 +3,7 @@
 namespace gbemu { namespace memory {
 
 ram::ram(gbemu::memory::as& as)
+  : high_ram_(127)
 {
   /*
   ** We do not support CGB mode at the moment, so we only have one RAM bank.
@@ -22,6 +23,18 @@ ram::ram(gbemu::memory::as& as)
   /* handlers for the 0xe000 - 0xfdff echo zone */
   as.add_read_handler({ 0xe000, 0xfdff }, rh);
   as.add_write_handler({ 0xe000, 0xfdff }, wh);
+
+  /* handler for the high RAM */
+  as.add_read_handler({ 0xff80, 0xfffe },
+    [&](uint16_t off) -> uint8_t {
+      return this->high_ram_[off];
+    }
+  );
+  as.add_write_handler({ 0xff80, 0xfffe },
+    [&](uint16_t off, uint8_t val) -> void {
+      this->high_ram_[off] = val;
+    }
+  );
 }
 
 }}
--- src/memory/ram.h
+++ src/memory/ram.h
@@ -17,6 +17,7 @@ class ram
 
   private:
     std::unordered_map<int, std::vector<uint8_t>> ram_banks_;
+    std::vector<uint8_t> high_ram_;
 };
 
 }}
